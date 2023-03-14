9fc1bc2ef582bc8369aed4d89160a71e92958171
h4cK3rM4n04 <eliemanoarm@gmail.com>
2023-03-14 21:32:40 +0100

    Les Listes

---
+++ #_16Listes.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+"""
+Chacun des éléments d'une liste à des indices!
+liste[:X] affiche les X premier éléments de la liste!
+liste[X:] affiche les X derniers éléments!
+liste[-X] affiche le Xème élément de la liste en partant de la fin!!!
+liste[A:B] affiche de l'élément d'indice A à l'indice B (avec B exclus)
+
+"""
+
+#Création d'une liste
+
+inventaire = ["arc", "fleche", "épée", "pistolet", "galil", "laser", "m14", "katana"]
+print(inventaire[-7:-3])
